35799124523b91029e36283e26a0fec9a8ec14ba
Youchen Lee <copyleft@utcr.org>
2014-07-01 21:09:55 +0800

    update connect.sh

--- connect-mirror.sh
+++ connect-mirror.sh
@@ -1,3 +1,8 @@
-EXTERNAL=DP3
-RES="1024x768"
+if [ -z "$1" ]; then
+    EXTERNAL=DP3
+    RES="1024x768"
+else
+    EXTERNAL=$1
+    RES=$2
+fi
 xrandr --output $EXTERNAL --mode "$RES" --output LVDS1 --mode "$RES" --same-as $EXTERNAL
--- connect.sh
+++ connect.sh
@@ -1,2 +1,6 @@
+if [ "$1" = "off" ]; then
+    xrandr --output LVDS1 --off
+fi
 xrandr --output HDMI3 --mode "1920x1080" --above LVDS1 --primary
 xrandr --output LVDS1 --mode "1366x768" --below HDMI3
+
